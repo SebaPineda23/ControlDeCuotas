cba703e9bc0ed878f36adb057f660bf08b4205d8
Pablo2800 <pablopalazzetti@gmail.com>
2024-03-18 17:01:52 -0300

    formulario listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import './App.css'
-
+import FormNewSocio from './views/FormNewSocio'
+import Home from './views/Home'
+import { Routes,Route} from 'react-router-dom'
 function App() {
   return (
-    <div></div>
+    <div className='bg-gray-400 w-full min-h-screen flex items-center justify-center'>
+      <Routes >
+        <Route path="/" element={<Home />} />
+        <Route path="/nuevoSocio" element={<FormNewSocio />} />
+      </Routes>
+    </div>
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Input } from "antd";
+const { Search } = Input;
 
+const onSearch = (value, _e) => console.log(value);
 export default function SearchBar() {
   return (
-    <div>SearchBar</div>
-  )
+    <div className="bg-white rounded-lg">
+      <Search
+        placeholder="Buscar un socio"
+        onSearch={onSearch}
+        allowClear
+        style={{
+          width: 200,
+        }}
+      />
+    </div>
+  );
 }
