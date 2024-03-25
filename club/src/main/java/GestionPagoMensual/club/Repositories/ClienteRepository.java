8cc4b21c6679f35c3676650f650ac32a1f58fe37
Pablo2800 <pablopalazzetti@gmail.com>
2024-03-25 16:58:03 -0300

    changes

--- club/src/main/java/GestionPagoMensual/club/Repositories/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package GestionPagoMensual.club.Repositories;
-
-import GestionPagoMensual.club.Entitys.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
