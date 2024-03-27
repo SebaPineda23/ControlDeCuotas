ee096283f745d8a2342831958031c5ce5d084f33
unknown <sebapine3004@gmail.com>
2024-03-27 13:34:55 -0300

    Actualizacion C_P_Mensual

--- src/main/java/GestionPagoMensual/club/Controllers/PagoMensualController.java
+++ src/main/java/GestionPagoMensual/club/Controllers/PagoMensualController.java
@@ -1,6 +1,8 @@
 package GestionPagoMensual.club.Controllers;
 
+import GestionPagoMensual.club.Entitys.Cliente;
 import GestionPagoMensual.club.Entitys.PagoMensual;
+import GestionPagoMensual.club.Services.ClienteService;
 import GestionPagoMensual.club.Services.PagoMensualService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("adm_clubes/pago_mensuales")
@@ -16,16 +19,25 @@ public class PagoMensualController {
 
     @Autowired
     private PagoMensualService pagoMensualService;
+    @Autowired
+    private ClienteService clienteService;
 
     @PostMapping("/{clienteId}/pagos")
     public ResponseEntity<String> crearPago(
             @PathVariable Long clienteId,
             @RequestBody PagoMensual nuevoPago
     ) throws Exception {
-        PagoMensual pagoCreado = pagoMensualService.guardarFacturaMensual(nuevoPago, clienteId);
-        return ResponseEntity.ok("Pago registrado exitosamente para el cliente con ID: " + clienteId +
-                ". ID del nuevo pago mensual: " + pagoCreado.getId());
+        try {
+            PagoMensual pagoCreado = pagoMensualService.guardarFacturaMensual(nuevoPago, clienteId);
+            return ResponseEntity.ok("Pago registrado exitosamente para el cliente con ID: " + clienteId +
+                    ". ID del nuevo pago mensual: " + pagoCreado.getId());
+        } catch (Exception e) {
+            // Handle exception appropriately, maybe log it
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al procesar la solicitud, " + e.getMessage());
+        }
     }
+
 
     @GetMapping
     public ResponseEntity<List<PagoMensual>> obtenerTodasLasFacturasMensuales() {
@@ -34,10 +46,13 @@ public class PagoMensualController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PagoMensual> obtenerFacturaMensualPorId(@PathVariable Long id) {
-        return pagoMensualService.obtenerFacturaMensualPorId(id)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<?> obtenerFacturaMensualPorId(@PathVariable Long id) {
+        Optional<PagoMensual> pagoMensual = pagoMensualService.obtenerFacturaMensualPorId(id);
+        if (pagoMensual.isPresent()) {
+            return ResponseEntity.ok(pagoMensual.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró ningun registro de pagos mensuales asociados al Cliente con el ID: " + id);
+        }
     }
 
     @DeleteMapping("/{id}")
