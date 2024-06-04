29338480af1caa20fa08eafa1525cfc0fb87a7ac
Pablo2800 <pablopalazzetti@gmail.com>
2024-06-03 21:14:22 -0300

    changes

--- FrontEnd/src/components/EditSocio.jsx
+++ FrontEnd/src/components/EditSocio.jsx
@@ -1,41 +1,37 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Button, Form, Input } from "antd";
 import { useSelector } from "react-redux";
 import { getFilterSocios } from "../redux/setSocios";
 import useFilters from "../hooks/useFilters";
+import Categorias from "./Categorias"; // Ajusta la ruta si es necesario
 
 export default function EditSocio() {
+  const [form] = Form.useForm();
   const socio = useSelector(getFilterSocios);
   const { edit } = useFilters();
-  const [initialValues, setInitialValues] = useState({
-    id: "",
-    nombre: "",
-    apellido: "",
-    email: "",
-    dni: "",
-    fecha_nacimiento: "",
-  });
+
   useEffect(() => {
-    setInitialValues({
+    form.setFieldsValue({
       id: socio.id,
       nombre: socio.nombre,
       apellido: socio.apellido,
       email: socio.email,
       dni: socio.dni,
       fecha_nacimiento: socio.fecha_nacimiento,
+      categoria: socio.categoria,
     });
-  }, [socio]);
+  }, [socio, form]);
 
   return (
     <div className="flex justify-center items-center w-11/12">
       <Form
+        form={form}
         className="bg-white rounded-lg p-5"
         style={{ minWidth: "300px" }}
         onFinish={edit}
-        initialValues={initialValues}
       >
         <Form.Item label="id" name="id">
-          {socio.id}
+          <span>{socio.id}</span>
         </Form.Item>
         <Form.Item
           name="nombre"
@@ -76,6 +72,15 @@ export default function EditSocio() {
           rules={[{ required: true, message: "Por favor ingrese una fecha" }]}
         >
           <Input type="date" />
+        </Form.Item>
+        <Form.Item
+          name="categoria"
+          label="Categoria"
+          rules={[
+            { required: true, message: "Por favor ingrese su categoria" },
+          ]}
+        >
+          <Categorias />
         </Form.Item>
         <div className="flex justify-center">
           <Form.Item>
